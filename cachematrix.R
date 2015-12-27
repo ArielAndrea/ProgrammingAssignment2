c3ba30694456424da67e69f539e3171b605456f6
ArielAndrea <ariel.m.andrea@gmail.com>
2015-12-26 21:20:47 -0600

    committing assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will make a matrix object that can have the inverse cached
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	xinv <- NULL # this is where the result of inversion is stored
+      # A setter function, use this to set a matrix to object created by makeCacheMatrix function
+      # e.g makeCacheMatrix(testmatrix) # here we work on testmatrix
+      # makeCacheMatrix$set(testmatrix1) # here we work on testmatrix1
+      
+	set <- function(y) {
+	  	x <<- y
+	 	xinv <<- NULL # it also initialises xinv to null
+      }
 
+      get <- function() {x} # return the input matrix
+      setInv <- function(inv) {xinv <<- inv} # set the inversed matrix
+      getInv <- function() {xinv} # return the inversed matrix
+      
+     
+	list(set = set, get = get,setInv = setInv, getInv = getInv)
+	# return a list that contains these functions
 }
 
 
-## Write a short comment describing this function
+
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getInv() # get the inversed matrix from object x
+      # it will be null if uncalculated, remember the first line "xinv <- NULL" in the previous function
+      
+	if(!is.null(m)) { # if the inversion result is there
+		message("retrieving cached data")
+		return(m) 
+      }
+      
+	data <- x$get() # if not, we do x$get to get the matrix object
+      m <- solve(data) # we solve it
+      x$setInv(m) # we then set it to the object
+      print(m) # print the solved result
 }
